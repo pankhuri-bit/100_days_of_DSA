440c9d75f6ece9e28a959462952321cd5da686b7
Pankhuri Tak <pankhuritak6@gmail.com>
2026-02-05 22:52:43 +0530

    Create q1.c

---
+++ Day5/q1.c
@@ -0,0 +1,40 @@
+/*Problem: A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. 
+  Your task is to create a single chronological log that preserves the correct order of arrivals.*/
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int p, q;
+    if (scanf("%d", &p) != 1) return 0;
+    int *log1 = (int *)malloc(p * sizeof(int));
+    for (int i = 0; i < p; i++) {
+        scanf("%d", &log1[i]);
+    }
+    if (scanf("%d", &q) != 1) return 0;
+    int *log2 = (int *)malloc(q * sizeof(int));
+    for (int i = 0; i < q; i++) {
+        scanf("%d", &log2[i]);
+    }
+    int i = 0;
+    int j = 0;
+    while (i < p && j < q) {
+        if (log1[i] <= log2[j]) {
+            printf("%d ", log1[i]);
+            i++;
+        } else {
+            printf("%d ", log2[j]);
+            j++;
+        }
+    }
+    while (i < p) {
+        printf("%d ", log1[i]);
+        i++;
+    }
+    while (j < q) {
+        printf("%d ", log2[j]);
+        j++;
+    }
+    printf("\n");
+    free(log1);
+    free(log2);
+    return 0;
+}
