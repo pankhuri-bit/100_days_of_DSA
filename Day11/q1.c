f0e54453b761f6739838977eaa8613ab9767fa6f
Pankhuri Tak <pankhuritak6@gmail.com>
2026-02-11 22:28:55 +0530

    Create q1.c

---
+++ Day11/q1.c
@@ -0,0 +1,39 @@
+// Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    
+    if (scanf("%d %d", &m, &n) != 2) return 0;
+
+    int first[m][n], second[m][n], sum[m][n];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &first[i][j]);
+        }
+    }
+
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &second[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            sum[i][j] = first[i][j] + second[i][j];
+
+            printf("%d", sum[i][j]);
+            if (j < n - 1) {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
