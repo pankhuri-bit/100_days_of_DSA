26ac3ceb21dd93a6ed1867a574e767afa64da5ad
Pankhuri Tak <pankhuritak6@gmail.com>
2026-02-24 22:44:56 +0530

    Update q1.c

--- Day15/q1.c
+++ Day15/q1.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main() {
-    int n;
-    if (scanf("%d", &n) != 1) return 0;
+    int m, n;
+    if (scanf("%d %d", &m, &n) != 2) return 0;
 
-    bool is_identity = true;
+    long long diagonal_sum = 0;
     int current_val;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &current_val);
+
             if (i == j) {
-                if (current_val != 1) {
-                    is_identity = false;
-                }
-            } 
-            else {
-                if (current_val != 0) {
-                    is_identity = false;
-                }
+                diagonal_sum += current_val;
             }
         }
     }
 
-    if (is_identity) {
-        printf("Identity Matrix\n");
-    } else {
-        printf("Not an Identity Matrix\n");
-    }
+    printf("%lld\n", diagonal_sum);
 
     return 0;
 }
