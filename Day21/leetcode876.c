400a4f0366a0b2ee27c694fec113353bb465426e
Pankhuri Tak <pankhuritak6@gmail.com>
2026-02-26 22:13:33 +0530

    Create leetcode876.c

---
+++ Day21/leetcode876.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;          
+        fast = fast->next->next;    
+    }
+    return slow;
+}
